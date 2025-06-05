569ed04dade8d758d7724334ac6000ab7b423310
Gustav Thilander <gustav.thilander@gmail.com>
2025-06-05 13:55:00 +0200

    auth jwt och swagger

---
+++ src/midleware/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/userModel.js';
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Ingen token.' });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findByPk(decoded.id);
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Ogiltig token.' });
+  }
+};
